7cfd23f1fcc5d2da1ee9b694d44c365677feaf33
jhkwebm <jhkwebm@mteletec.com>
2025-07-31 08:22:27 +0900

    Initial commit: Basic React project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MTT Framework Prototype</h1>
+        <p>Drag and Drop UI Builder</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
